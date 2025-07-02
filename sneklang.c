f00fe3e44adcbfb7d8b03d6ead8e08bccf42f341
GitJohnFis <jfisiiahi69@gmail.com>
2025-07-02 01:31:13 +0000

    Add PrintVars and fixbugs example programs; implement Sneklang interpreter

---
+++ sneklang.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Starting the Sneklang interpreter...\n");
+    return 0;
+}
+
+// int main() {
+//     printf("Starting the Sneklang interpreter..\n");
+//     return 0;
+// }
